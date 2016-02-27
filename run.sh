f7c9a2484cd25a4c4061eb7476028aad0be4c96c
Joni Kurunsaari <tabasko@kapsi.fi>
2016-02-27 22:48:28 +0200

    Added comments, fixed rm -rf

--- run.sh
+++ run.sh
@@ -1,10 +1,38 @@
-#! /bin/sh
+#!/bin/bash
 
-cd ./build/
+#Stop script if any command gives error after run
+set -e
 
-rm * -Rf
-autovala update
+#Set full path for sys commands
+RM=$(which rm)
+MKDIR=$(which mkdir)
+AUTOVALA=$(which autovala)
+CMAKE=$(which cmake)
+MAKE=$(which make)
+#Set build dir
+BUILD_DIR="./build/"
 
-cmake ..
-make
+#Check if BUILD_DIR exists and is empty. Prompt user before removing it
+if [[ -d "$BUILD_DIR" && "$(ls -A $BUILD_DIR)" ]]; then
+   echo  -e "WARNING: $BUILD_DIR exists and is not empty!"
+   read -p "Overwrite? (Y/N)" -n 1 -r
+   echo
+   if [[ $REPLY =~ ^[Yy]$ ]]; then
+      $RM -Rv $BUILD_DIR/*
+   else
+      echo "Aborted."
+      exit 1
+   fi
+else
+   #Create BUILD_DIR
+   $MKDIR -p $BUILD_DIR
+fi
+
+#RUN AUTOVALA & MAKE PROJECT
+cd $BUILD_DIR
+$AUTOVALA update
+$CMAKE ..
+$MAKE
+
+#RUN PROGRAM
 ./src/telefax
