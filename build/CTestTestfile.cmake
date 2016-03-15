cd4389850bcb1bc0d1f2de57ef186d0e9680f167
Jussi Virtanen <virtanenjt@gmail.com>
2016-03-15 10:42:23 +0200

    Trying to put the build directory into .gitignore

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jtvi/Projects/telefax
-# Build directory: /home/jtvi/Projects/telefax/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(src)
-SUBDIRS(po)
-SUBDIRS(schemas)
--- build/schemas/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jtvi/Projects/telefax/schemas
-# Build directory: /home/jtvi/Projects/telefax/build/schemas
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
