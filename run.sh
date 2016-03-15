270971f1c47dfaf796500356b342a9ec3bc4c078
Jussi <virtanenjt@gmail.com>
2016-03-15 10:15:42 +0200

    Created VSCode project and added some missing files.

--- run.sh
+++ run.sh
@@ -3,30 +3,48 @@
 #Stop script if any command gives error after run
 set -e
 
+COMMAND_ARGUMENT="$1"
+BUILD_DIR=$(pwd)"/build/"
+RUN_DIR=$(pwd)"/build/bin/"
 #Set full path for sys commands
-RM=$(which rm)
-MKDIR=$(which mkdir)
-AUTOVALA=$(which autovala)
-CMAKE=$(which cmake)
-MAKE=$(which make)
-#Set build dir
-BUILD_DIR="./build/"
 
 #Check if BUILD_DIR exists and is empty. Prompt user before removing it
-if [[ -d "$BUILD_DIR" && "$(ls -A $BUILD_DIR)" ]]; then
-   echo  -e "WARNING: Cleaning $BUILD_DIR!"
-   $RM -Rv $BUILD_DIR/*
-   else
-   #Create BUILD_DIR
-   $MKDIR -p $BUILD_DIR
-fi
+Clear (){
+    RM=$(which rm)
+    MKDIR=$(which mkdir)
+    echo "Clearing build directory"
+    if [[ -d "$BUILD_DIR" && "$(ls -A $BUILD_DIR)" ]]; then
+        echo  -e "WARNING: Cleaning $BUILD_DIR!"
+        $RM -Rv $BUILD_DIR/*
+    else
+        #Create BUILD_DIR
+        $MKDIR -p $BUILD_DIR
+    fi
+}
 
-#RUN AUTOVALA & MAKE PROJECT
-cd $BUILD_DIR
-$AUTOVALA po
-$AUTOVALA update
-$CMAKE ..
-$MAKE
+Build (){
+    cd $BUILD_DIR
+    AUTOVALA=$(which autovala)
+    CMAKE=$(which cmake)
+    MAKE=$(which make)
+    
+    $AUTOVALA update
+    $CMAKE -DCMAKE_BUILD_TYPE=Debug .. -DCMAKE_INSTALL_PREFIX=$RUN_DIR .. 
+    $MAKE
+    $MAKE install
+}
 
-#RUN PROGRAM
-./src/telefax
+UpdateLanguageFiles(){
+    $AUTOVALA po
+}
+
+case "$1" in
+        clear) Clear
+            ;;
+        build) Clear && Build
+            ;;
+        update-language-files) Clear && UpdateLanguageFiles && Build
+            ;;
+        *) echo "Bad command argument $1"
+            ;;
+esac
